5c83e0ced0d26b741894aa85c9adb948f18944de
Nolan Welch <bugygamer.mc@gmail.com>
2023-01-10 20:56:52 -0700

    commit tree 0c3e6fbb288cc212d182fa1390e7cb735fe7898f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name){
+  cats.push (name);
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift (name)
+}
+function  destructivelyRemoveFirstCat(name){
+  cats.shift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+  cats.pop(name)
+}
+
+function appendCat (name){
+  const cats1=[...cats];
+  cats1.push(name)
+}
+
+function prependCat(name){
+
+}
+
+function removeLastCat(){
+
+}
+function removeFirstCat(){
+
+
+
+}
